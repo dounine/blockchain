c621fba079e403cf51bdc4f8fa1f2171fef067e2
huanghuanlai <102535481@qq.com>
2017-03-15 20:53:46 +0800

    asdf

---
+++ src/main/java/com/dounine/blockchain/App.java
@@ -0,0 +1,14 @@
+package com.dounine.blockchain;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by lake on 17-2-28.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        StringUtils.isBlank("");
+        System.out.println("hello world.");
+    }
+}

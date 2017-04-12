c62d114d3d47665bc4cd77a0bbacbfc3442a6693
huanghuanlai <102535481@qq.com>
2017-04-12 15:20:58 +0800

    a

--- src/main/java/com/dounine/blockchain/App.java
+++ src/main/java/com/dounine/blockchain/App.java
@@ -2,13 +2,17 @@ package com.dounine.blockchain;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by lake on 17-2-28.
  */
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         StringUtils.isBlank("");
         System.out.println("hello world.");
+        KeyPairGenerator.getInstance("RSA");
     }
 }
